a3b861e62cce42acb271d89a4d7aa458152f00b6
ThisIsAmazing <86086760+ThisIsAmazing@users.noreply.github.com>
2022-06-10 20:51:14 -0500

    Add files via upload
    The 5 Unity scripts used to make the obstacle course

---
+++ Mover.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]float moveSpeed = 5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintInstructions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Move the player using WASD or Arrow Keys.");
+        Debug.Log("Do not hit the objects, have fun!");
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue,0,zValue);
+    }
+}
